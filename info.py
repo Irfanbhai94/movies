365139c025fd8fe3b97e4ddc37c70858f6f7b10b
Irfanbhai94 <asifirfan85756@gmail.com>
2024-12-19 17:22:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '8108550750:AAEzeXr1RAeb5wxWddg_dNo5IP8ANrZ
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7244446698').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Supermanremake") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1002486915650'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002486915650'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviegroupli')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002344221556').split()]
 #---------------------------------------------------------------
